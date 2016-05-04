61aacecb6849ad25dffcb5c261a69b6e0d851203
nayed.saidali@gmail.com <nayed.saidali@gmail.com>
2016-05-04 10:07:32 +0200

    views

--- random-number-app/ViewController.swift
+++ random-number-app/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userNumberLabel: UILabel!
+    @IBOutlet weak var appNumberLabel: UILabel!
+    @IBOutlet weak var guessBtn: UIButton!
+    @IBOutlet weak var playAgainBtn: UIButton!
+    
+    @IBAction func playAgainAction(sender: AnyObject) {
+        playAgainBtn.hidden = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
